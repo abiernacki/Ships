f7899637e7721f9c2812a55f26e2f593e37cbbb6
Adrian Biernacki <adrianbie86@gmail.com>
2019-08-05 21:14:29 +0200

    Ships initial

---
+++ src/test/java/StatkiTest.java
@@ -0,0 +1,18 @@
+import Ships.Statki;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StatkiTest {
+
+    @Test
+    public void testPlansza(){
+
+        //given
+        Statki statki = new Statki();
+
+        //when
+        statki.plansza();
+    }
+
+}
