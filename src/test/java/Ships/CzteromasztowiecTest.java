24f8d8239a34bdfa1832d0602e244f46e9ae9c60
Adrian Biernacki <adrianbie86@gmail.com>
2019-08-14 07:19:13 +0200

    Czteromasztowiec test

--- src/test/java/Ships/CzteromasztowiecTest.java
+++ src/test/java/Ships/CzteromasztowiecTest.java
@@ -4,16 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class TrzymasztowiecTest {
+public class CzteromasztowiecTest {
 
     @Test
     public void TestPoprawny1(){
 
         //given
-        Trzymasztowiec trzymasztowiec = new Trzymasztowiec(1,1,1,2,1,3);
+        Czteromasztowiec czteromasztowiec = new Czteromasztowiec(1,1,1,2,1,3,1,4);
 
         //when
-        boolean result = trzymasztowiec.validate();
+        boolean result = czteromasztowiec.validate();
 
         //then
 
@@ -24,10 +24,10 @@ public class TrzymasztowiecTest {
     public void TestPoprawny2(){
 
         //given
-        Trzymasztowiec trzymasztowiec = new Trzymasztowiec(1,1,2,1,3,1);
+        Czteromasztowiec czteromasztowiec = new Czteromasztowiec(1,1,2,1,3,1,4,1);
 
         //when
-        boolean result = trzymasztowiec.validate();
+        boolean result = czteromasztowiec.validate();
 
         //then
 
@@ -38,10 +38,10 @@ public class TrzymasztowiecTest {
     public void TestNiePoprawny1(){
 
         //given
-        Trzymasztowiec trzymasztowiec = new Trzymasztowiec(2,3,4,5,6,7);
+        Czteromasztowiec czteromasztowiec = new Czteromasztowiec(1,1,1,3,1,5,1,7);
 
         //when
-        boolean result = trzymasztowiec.validate();
+        boolean result = czteromasztowiec.validate();
 
         //then
 
@@ -52,10 +52,10 @@ public class TrzymasztowiecTest {
     public void TestNiePoprawny2(){
 
         //given
-        Trzymasztowiec trzymasztowiec = new Trzymasztowiec(1,1,1,3,1,5);
+        Czteromasztowiec czteromasztowiec = new Czteromasztowiec(1,1,2,3,4,5,1,7);
 
         //when
-        boolean result = trzymasztowiec.validate();
+        boolean result = czteromasztowiec.validate();
 
         //then
 
@@ -66,14 +66,13 @@ public class TrzymasztowiecTest {
     public void TestNiePoprawny3(){
 
         //given
-        Trzymasztowiec trzymasztowiec = new Trzymasztowiec(1,1,1,2,2,2);
+        Czteromasztowiec czteromasztowiec = new Czteromasztowiec(1,1,1,2,1,3,2,3);
 
         //when
-        boolean result = trzymasztowiec.validate();
+        boolean result = czteromasztowiec.validate();
 
         //then
 
         assertFalse(result);
     }
-
 }
